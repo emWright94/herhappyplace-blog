af583f26fb3b48740e0ac6ed52701a8cd8a6ad76
emWright94 <emwright94@gmail.com>
2025-04-25 16:21:43 -0400

    added to bus

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
-let topButton = document.querySelector("#topButton");
 let rootElement = document.documentElement;
-
 function topFunction() {
+  console.log("test");
+
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 }
-
+let topButton = document.querySelector("#topButton");
 topButton.addEventListener("click", topFunction);
+
+console.log("testing testing");
